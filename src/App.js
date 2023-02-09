7c0c3ab22557cae6a84963ada8b7b160bdf634d7
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-02-09 23:06:01 +0400

    Extract expenses into separate component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './index.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -27,26 +27,7 @@ function App() {
   return (
     <div className="App">
       <h1>Money Tracker</h1>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses expenses={expenses} />
     </div>
   );
 }
